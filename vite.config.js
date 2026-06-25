3e305f9853e2548f4bcc0ada0fd15487fee3fc57
Adeyemi Favour <adeyemifavour615@gmail.com>
2026-06-25 14:29:06 +0100

    Update base path to match repo name

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-first-react-app/', // Make sure this matches your exact GitHub repo name
+  base: '/Haywhytech/', // Change this to your exact repo name with slashes
 })

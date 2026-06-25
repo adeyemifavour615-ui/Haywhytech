c1e07665ccd6d306d0136b89b1f62e767efdb6c0
Adeyemi Favour <adeyemifavour615@gmail.com>
2026-06-25 14:17:49 +0100

    Configure deployment settings

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/my-first-react-app/', // Make sure this matches your exact GitHub repo name
 })
